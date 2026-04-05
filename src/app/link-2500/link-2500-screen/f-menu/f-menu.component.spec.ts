3884aaa889a381e8f67ded2df08c07d8b5bde2bd
Tenlive1 <nguyenperry99@gmail.com>
2026-04-05 11:26:22 -0500

    able to add routes on the main page so it is linked to Link 2500

---
+++ src/app/link-2500/link-2500-screen/f-menu/f-menu.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FMenuComponent } from './f-menu.component';
+
+describe('FMenuComponent', () => {
+  let component: FMenuComponent;
+  let fixture: ComponentFixture<FMenuComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FMenuComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/link-2500/link-2500-screen/f-menu/f-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-f-menu',
+  imports: [],
+  templateUrl: './f-menu.component.html',
+  styleUrl: './f-menu.component.css'
+})
+export class FMenuComponent {
+
+}
