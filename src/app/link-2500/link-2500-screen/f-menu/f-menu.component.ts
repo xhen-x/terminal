e1b96afe11245b63512df03b924988f1a9a25a2d
Tenlive1 <nguyenperry99@gmail.com>
2026-05-05 21:30:49 -0500

    added header to f menu

--- src/app/link-2500/link-2500-screen/f-menu/f-menu.component.ts
+++ src/app/link-2500/link-2500-screen/f-menu/f-menu.component.ts
@@ -3,10 +3,11 @@ import { Link2500PinpadService } from '../../link-2500-pinpad/link-2500-pinpad.s
 import { Subscription } from 'rxjs';
 import { Router } from "@angular/router";
 import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "../../../header/header.component";
 
 @Component({
   selector: 'app-F-menu',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './F-menu.component.html',
   styleUrl: './F-menu.component.css'
 })
